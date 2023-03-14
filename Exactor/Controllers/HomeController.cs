51dd8936aa892ff469ceb52e4656efc7786fc210
R3DM00NS5222 <66016502+R3DM00NS5222@users.noreply.github.com>
2023-03-14 19:03:11 -0400

    Add project files.

---
+++ Exactor/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Exactor.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Exactor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DatingList()
+        {
+            return View();
+        }
+
+        public IActionResult OpenerList()
+        {
+            return View();
+        }
+
+        public IActionResult Tasks()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Exactor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Exactor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
